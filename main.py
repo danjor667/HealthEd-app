7e3702375791d067eb1a6e5ddd4e76c1d6f68038
MyUser <f.akinwande@alustudent.com>
2023-08-05 11:11:16 -0700

    PLD 3 Task

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ def load_quiz_data(filename):
     with open(filename, "r") as file:
         quiz_data = eval(file.read())
     return quiz_data
+def display_categories(quiz_data):
+    print("Select a category:")
+    for index, category in enumerate(quiz_data.keys(), start=1):
+        print(f"{index}. {category}")
