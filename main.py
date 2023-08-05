b4ac2a5c5fca23a797d90b9a191141ef83a84855
MyUser <f.akinwande@alustudent.com>
2023-08-05 11:08:40 -0700

    PLD 3

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 """
 main function
 """
+def load_quiz_data(filename):
+    with open(filename, "r") as file:
+        quiz_data = eval(file.read())
+    return quiz_data
