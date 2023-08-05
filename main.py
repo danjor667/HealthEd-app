835781bc2da248ab76e2e5ca9ed88c10b986a2c3
danjor667 <j.nguepi@alustudent.com>
2023-08-05 11:53:35 -0700

    fixing bugs

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """
-main function
+main
 """
+
+import random
+
 def load_quiz_data(filename):
     with open(filename, "r") as file:
         quiz_data = eval(file.read())
@@ -11,6 +14,7 @@ def display_categories(quiz_data):
     print("Select a category:")
     for index, category in enumerate(quiz_data.keys(), start=1):
         print(f"{index}. {category}")
+
 def select_category(quiz_data):
     while True:
         choice = int(input("Enter the category number: "))
@@ -20,6 +24,52 @@ def select_category(quiz_data):
             return selected_category
         else:
             print("Invalid category number. Please try again.")
+
 def get_random_question(quiz_data, category):
     questions = quiz_data[category]
     return random.choice(questions)
+
+def present_question(question):
+    print(question["question"])
+    for option in question["options"]:
+        print(option)
+
+def get_user_answer():
+    while True:
+        user_answer = input("Enter your answer (A, B, C, or D), or type 'exit' to quit: ").upper()
+        if user_answer in ["A", "B", "C", "D", "EXIT"]:
+            return user_answer
+        else:
+            print("Invalid answer. Please choose A, B, C, or D.")
+
+def check_answer(question, user_answer):
+    correct_answer = question["answer"]
+    if user_answer == correct_answer:
+        print("Correct answer!")
+    else:
+        print("Incorrect answer. The correct answer is", correct_answer)
+
+def play_quiz_game(quiz_data):
+    print("Welcome to the Health Education Quiz Game!")
+    display_categories(quiz_data)
+
+    selected_category = select_category(quiz_data)
+    print(f"\nYou have selected the '{selected_category}' category.\n")
+
+    question = get_random_question(quiz_data, selected_category)
+    present_question(question)
+
+    user_answer = get_user_answer()
+    if user_answer == "EXIT":
+        print("Exiting the program...")
+        return
+    check_answer(question, user_answer)
+
+quiz_data = load_quiz_data("quiz_questions.txt")
+
+while True:
+    play_quiz_game(quiz_data)
+    continue_playing = input("Do you want to play again? (y/n): ")
+    if continue_playing.lower() != "y":
+        print("Exiting the program...")
+        break
